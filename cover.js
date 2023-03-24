f4e41e70daf775d7fc57902aad036806ac0aacfc
pratichiparida <114859819+pratichiparida@users.noreply.github.com>
2023-03-24 21:21:59 +0530

    Add files via upload

---
+++ cover.js
@@ -0,0 +1,27 @@
+const inputs = form.elements;
+const form = document.getElementById('form');
+
+for (i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener('invalid', function () {
+    form.className = 'submitted';
+  });
+};
+
+function checkPassword() {
+  const password = document.getElementById('password').value;
+  const passwordConfirm = document.getElementById('password-confirm').value;
+  const passwordInput = document.getElementById('password');
+  const passwordConfirmInput = document.getElementById('password-confirm');
+  
+  if (password == passwordConfirm) {
+    passwordInput.style.borderColor = 'green';
+    passwordConfirmInput.style.borderColor = 'green';
+  } else if (password =! '' && passwordConfirm == ''){
+    return;
+  } else {
+    passwordInput.style.borderColor = 'red';
+    passwordConfirmInput.style.borderColor = 'red';
+  }
+}
+
+
